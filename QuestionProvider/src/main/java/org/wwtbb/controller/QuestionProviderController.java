211dd2b3e0cee805ebf1d1cbb7b6a05eaeedadc4
Vishal V <vishalvyavahare@gmail.com>
2023-09-30 15:07:07 +0200

    dummy function to check if coverage result changes.

--- QuestionProvider/src/main/java/org/wwtbb/controller/QuestionProviderController.java
+++ QuestionProvider/src/main/java/org/wwtbb/controller/QuestionProviderController.java
@@ -3,7 +3,6 @@ package org.wwtbb.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wwtbb.data.QuestionProviderDTO;
@@ -16,9 +15,6 @@ public class QuestionProviderController {
     private final Logger logger = LoggerFactory.getLogger(QuestionProviderController.class);
 
     @Autowired
-    private Environment environment;
-
-    @Autowired
     private QuestionProviderService questionProviderService;
 
 
@@ -26,11 +22,11 @@ public class QuestionProviderController {
     public List<QuestionProviderDTO> getAllQuestions() {
         List<QuestionProviderDTO> questions = questionProviderService.getAllQuestions();
         logger.info("All question => {}", questions);
-
+        fn();
         return questions;
     }
 
-    private void fn(){
+    private void fn() {
         int c = 5;
         int b = 2;
     }
